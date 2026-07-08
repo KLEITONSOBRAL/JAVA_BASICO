a32a0657d3de1d2227d70679e869d97798eef58d
Kleiton Sobral <kleiton.c@aluno.senai.br>
2026-07-08 05:51:59 -0300

    Refactor sum calculation and output formatting

--- JAVA_BASICO_1/EXERCICIO_3_KLEITON/src/main/java/com/mycompany/mavenproject1/EXERCICIO_3_KLEITON.java
+++ JAVA_BASICO_1/EXERCICIO_3_KLEITON/src/main/java/com/mycompany/mavenproject1/EXERCICIO_3_KLEITON.java
@@ -1,21 +1,37 @@
+/*Faça um programa em Java que leia dois números inteiros digitados pelo usuário.
+O programa deve calcular e exibir a soma dos números apenas se os dois forem do mesmo tipo de sinal,
+ou seja, ambos positivos ou iguais a zero, ou ambos negativos.
+Caso um número seja positivo ou zero e o outro seja negativo, exiba a mensagem:
+Dados de entrada sao invalidos
+A saída da soma deve seguir o formato:
+Soma de N1 com N2 = RESULTADO
+*/
+
 package com.mycompany.mavenproject1;
 
 import java.util.Scanner;
 
 public class EXERCICIO_3_KLEITON {
+    
     public static void main(String[] args) {
+        
         Scanner scanner = new Scanner(System.in);
-        
-        int num1 = scanner.nextInt();
-        int num2 = scanner.nextInt();
-        
+
+        int num1 = 0,num2 = 0, resultado = 0;
+
+        System.out.println("Digite o primeiro número: ");
+        num1 = scanner.nextInt();
+
+        System.out.println("Digite o segundo número: ");
+        num2 = scanner.nextInt();
+
         if ((num1 >= 0 && num2 >= 0) || (num1 < 0 && num2 < 0)) {
-            int soma = num1 + num2;
-            System.out.println("Soma de " + num1 + " com " + num2 + " = " + soma);
+            resultado = num1 + num2;
+            System.out.println("Soma de " + num1 + " com " + num2 + " = " + resultado);
         } else {
             System.out.println("Dados de entrada sao invalidos");
         }
-        
+
         scanner.close();
     }
 }
