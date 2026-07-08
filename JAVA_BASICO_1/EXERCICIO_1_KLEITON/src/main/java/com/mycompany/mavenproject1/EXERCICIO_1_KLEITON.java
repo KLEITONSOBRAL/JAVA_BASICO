b364ee17380b74936c5580445d45de1c25774b28
Kleiton Sobral <kleiton.c@aluno.senai.br>
2026-07-08 05:21:23 -0300

    Update EXERCICIO_1_KLEITON.java

--- JAVA_BASICO_1/EXERCICIO_1_KLEITON/src/main/java/com/mycompany/mavenproject1/EXERCICIO_1_KLEITON.java
+++ JAVA_BASICO_1/EXERCICIO_1_KLEITON/src/main/java/com/mycompany/mavenproject1/EXERCICIO_1_KLEITON.java
@@ -1,16 +1,29 @@
+/*Faça um programa em Java que leia dois números inteiros digitados pelo usuário, 
+calcule a soma entre eles e exiba o resultado na tela no seguinte formato:
+resultado de N1 + N2 = RESULTADO
+*/
+
 package com.mycompany.mavenproject1;
 
 import java.util.Scanner;
 
 public class EXERCICIO_1_KLEITON {
+    
     public static void main(String[] args) {
+        
         Scanner scanner = new Scanner(System.in);
-        
-        int num1 = scanner.nextInt();
-        int num2 = scanner.nextInt();
-        
-        int soma = num1 + num2;
-        System.out.println("Soma de " + num1 + " com " + num2 + " = " + soma);
+
+        int num1 = 0, num2 = 0, resultado = 0;
+
+        System.out.println("Digite o primeiro número;");
+        num1 = scanner.nextInt();
+
+        System.out.println("Digite o segundo número:");
+        num2 = scanner.nextInt();
+
+        resultado = num1 + num2;
+
+        System.out.println("O resultado de " + num1 + " + " + num2 + " = " + resultado);
         
         scanner.close();
     }
