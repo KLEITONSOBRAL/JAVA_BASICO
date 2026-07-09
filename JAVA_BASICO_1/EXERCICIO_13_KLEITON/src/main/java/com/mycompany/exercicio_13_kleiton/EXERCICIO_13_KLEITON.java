9b1a0e7003450f935dbea9495febcf6d6334ea07
Kleiton Sobral <kleiton.c@aluno.senai.br>
2026-07-09 06:53:56 -0300

    Implement program to print numbers from 150 to 300
    Added a Java program to display integers from 150 to 300.

--- JAVA_BASICO_1/EXERCICIO_13_KLEITON/src/main/java/com/mycompany/exercicio_13_kleiton/EXERCICIO_13_KLEITON.java
+++ JAVA_BASICO_1/EXERCICIO_13_KLEITON/src/main/java/com/mycompany/exercicio_13_kleiton/EXERCICIO_13_KLEITON.java
@@ -1,7 +1,13 @@
+/*
+Faça um programa em Java que exiba na tela todos os números inteiros de 150 até 300, um por linha.
+*/
+
 package com.mycompany.exercicio_13_kleiton;
 
 public class EXERCICIO_13_KLEITON {
+    
     public static void main(String[] args) {
+        
         for (int i = 150; i <= 300; i++) {
             System.out.println(i);
         }
