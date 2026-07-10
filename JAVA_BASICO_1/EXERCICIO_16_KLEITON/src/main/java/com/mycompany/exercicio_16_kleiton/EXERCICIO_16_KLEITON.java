c9c927da4a3aa2698141d3054a207790b5cb8857
Kleiton Sobral <kleiton.c@aluno.senai.br>
2026-07-10 07:04:14 -0300

    Refactor output messages and improve formatting

--- JAVA_BASICO_1/EXERCICIO_16_KLEITON/src/main/java/com/mycompany/exercicio_16_kleiton/EXERCICIO_16_KLEITON.java
+++ JAVA_BASICO_1/EXERCICIO_16_KLEITON/src/main/java/com/mycompany/exercicio_16_kleiton/EXERCICIO_16_KLEITON.java
@@ -1,35 +1,47 @@
+/*
+Faça um programa em Java que leia a quantidade de funcionários de uma empresa.
+Em seguida, leia o salário de cada funcionário e armazene esses valores em um vetor.
+Depois, calcule a média salarial da empresa e informe quantos funcionários recebem
+salário acima da média.
+Ao final, exiba:
+A media salarial e: MEDIA
+Numero de funcionarios que ganham acima da media: QUANTIDADE
+*/
+
 package com.mycompany.exercicio_16_kleiton;
 
 import java.util.Scanner;
 
 public class EXERCICIO_16_KLEITON {
+    
     public static void main(String[] args) {
+        
         Scanner scanner = new Scanner(System.in);
-        
-        System.out.print("Digite o numero de funcionarios da empresa: ");
+
+        System.out.println("Digite o número de funcionarios da empresa: ");
         int numFuncionarios = scanner.nextInt();
-        
+
         double[] salarios = new double[numFuncionarios];
         double somaSalarios = 0;
-        
+
         for (int i = 0; i < numFuncionarios; i++) {
-            System.out.print("Digite o salario do funcionario " + (i + 1) + ": ");
+            System.out.println("Digite o salario do funcionario " + (i + 1) + ": ");
             salarios[i] = scanner.nextDouble();
             somaSalarios += salarios[i];
         }
-        
+
         double mediaSalarial = somaSalarios / numFuncionarios;
         int acimaDaMedia = 0;
-        
+
         for (int i = 0; i < numFuncionarios; i++) {
             if (salarios[i] > mediaSalarial) {
                 acimaDaMedia++;
             }
         }
-        
-        System.out.println("A media salarial e: " + mediaSalarial);
-        System.out.println("Numero de funcionarios que ganham acima da media: " + acimaDaMedia);
-        
+
+        System.out.println("A média salarial é: " + mediaSalarial);
+        System.out.println("Número de funcionarios que ganham acima da média: " + acimaDaMedia);
+
         scanner.close();
     }
 }
