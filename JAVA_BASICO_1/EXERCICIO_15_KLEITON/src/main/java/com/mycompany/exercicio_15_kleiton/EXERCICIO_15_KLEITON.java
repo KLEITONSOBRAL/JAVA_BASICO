0948df4c04b75060fabd255d531c159d4c091ac8
Kleiton Sobral <kleiton.c@aluno.senai.br>
2026-07-10 06:44:07 -0300

    Fix output messages and formatting in Java program

--- JAVA_BASICO_1/EXERCICIO_15_KLEITON/src/main/java/com/mycompany/exercicio_15_kleiton/EXERCICIO_15_KLEITON.java
+++ JAVA_BASICO_1/EXERCICIO_15_KLEITON/src/main/java/com/mycompany/exercicio_15_kleiton/EXERCICIO_15_KLEITON.java
@@ -1,21 +1,30 @@
+/*
+Faça um programa em Java que leia o ano de nascimento de uma pessoa e o ano atual.
+Em seguida, calcule a idade da pessoa no ano atual e exiba o resultado na tela.
+A saída deve seguir o formato:
+A idade do usuario no ano atual é: IDADE anos.
+*/
+
 package com.mycompany.exercicio_15_kleiton;
 
 import java.util.Scanner;
 
 public class EXERCICIO_15_KLEITON {
+    
     public static void main(String[] args) {
+        
         Scanner scanner = new Scanner(System.in);
-        
-        System.out.print("Digite o ano de nascimento: ");
+
+        System.out.println("Digite o ano de nascimento: ");
         int anoNascimento = scanner.nextInt();
-        
-        System.out.print("Digite o ano atual: ");
+
+        System.out.println("Digite o ano atual: ");
         int anoAtual = scanner.nextInt();
-        
+
         int idade = anoAtual - anoNascimento;
-        
-        System.out.println("A idade do usuario no ano atual e: " + idade + " anos.");
-        
+
+        System.out.println("A idade do usuario no ano atual é " + idade + " anos.");
+
         scanner.close();
     }
 }
